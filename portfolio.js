1117a29d1265bad5b2cfb8980f706736c8d1f3f7
Sivasankari <nvssiva03@gmail.com>
2025-01-30 12:11:54 +0530

    Update with latest changes

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+import 'boxicons'
+
 
     // Toggle navbar on mobile view
     let menuIcon = document.querySelector('#icons');
